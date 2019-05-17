d0f205012e5adef356dbd11a86997a0e166290fc
zhaoshuai3 <1115870494@qq.com>
2019-05-17 18:29:30 +0800

    change redis cli

--- rediscli/util.go
+++ rediscli/util.go
@@ -1,18 +1,34 @@
 package rediscli
 
-import "github.com/garyburd/redigo/redis"
+import (
+	"github.com/garyburd/redigo/redis"
+	"strings"
+)
 
-func ExecCmd(cmd string){
+var ip string
+var port string
+var pwd string
 
+var c redis.Conn
+
+func ExecCmd(cmdStr string) (interface{},error){
+	if c == nil{
+		checkRedis()
+	}
+	cmds := strings.Split(cmdStr," ")
+	cmd := cmds[0]
+	args := cmds[1:]
+	return c.Do(cmd,args)
 }
 
 func checkRedis() error{
-	c, err := redis.Dial("tcp", "localhost:6379")
-	redis.Dial()
+	pwd := redis.DialPassword(pwd)
+	conn, err := redis.Dial("tcp", ip+":"+port,pwd)
 	if err != nil{
-
+		return err
 	}
-
+	c = conn
+	return nil
 }
 
 
